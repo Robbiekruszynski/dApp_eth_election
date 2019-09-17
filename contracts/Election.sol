547f0fb29d7d396e907c2498e03287feab31a584
robbie <robbiekruszynski@gmail.com>
2019-09-17 00:59:16 -0700

    add vote functions

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,9 +7,10 @@ contract Election {
   struct Candidate {
     uint id;
     string name;
-    uint VoteCount;
+    uint voteCount;
   }
-
+  //store accounts that have voted
+  mapping(address => bool) public voters;
   //store candidates
   //fetch candidate
   mapping(uint => Candidate) public candidates;
@@ -17,6 +18,12 @@ contract Election {
   //store candidates
   //allows you to determine how many candidates there are and fetch them if needed
   uint public candidatesCount;
+
+  //voted event
+  event votedEvent (
+    uint indexed _candidateId
+
+  );
 
 
     // Constructor
@@ -26,7 +33,24 @@ contract Election {
     }
 
     function addCandidate (string memory _name) private {
-      candidatesCount ++;
-      candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
