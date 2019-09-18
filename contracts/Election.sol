61bcbfaa4e5077d4a4c2be99c50021eb4939ed16
robbie <robbiekruszynski@gmail.com>
2019-09-18 13:42:28 -0700

    work on refresh issues

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,37 +2,32 @@ pragma solidity ^0.5.0;
 
 
 contract Election {
-
-  //model a Candidate
-  struct Candidate {
-    uint id;
-    string name;
-    uint voteCount;
-  }
-  //store accounts that have voted
-  mapping(address => bool) public voters;
-  //store candidates
-  //fetch candidate
-  mapping(uint => Candidate) public candidates;
-
-  //store candidates
-  //allows you to determine how many candidates there are and fetch them if needed
-  uint public candidatesCount;
-
-  //voted event
-  event votedEvent (
-    uint indexed _candidateId
-
-  );
-
-
-    // Constructor
-    constructor () public {
-      addCandidate("Candidate 1");
-      addCandidate("Candidate 2");
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
 
-    function addCandidate (string memory _name) private {
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
@@ -53,4 +48,3 @@ contract Election {
         // trigger voted event
         emit votedEvent(_candidateId);
     }
-}
